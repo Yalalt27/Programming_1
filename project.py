6acb4327946d7d8da85c0a8cebf3482167ec6ecf
Yalalt27 <yalaltbatochirkhuyag@gmail.com>
2026-04-17 14:47:02 +0200

    Update project.py

---
+++ project.py
@@ -0,0 +1,132 @@
+import pygame
+import time
+import random
+
+pygame.init()
+
+# Өрөөний хэмжээ
+width = 600
+height = 400
+window = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Snake Game")
+
+# Өнгөнүүд
+white = (255, 255, 255)
+yellow = (255, 255, 102)
+black = (0, 0, 0)
+red = (213, 50, 80)
+green = (0, 255, 0)
+blue = (50, 153, 213)
+
+clock = pygame.time.Clock()
+snake_block = 10
+snake_speed = 15
+
+font_style = pygame.font.SysFont("bahnschrift", 25)
+score_font = pygame.font.SysFont("bahnschrift", 30)
+
+
+def score(score):
+    value = score_font.render("Score: " + str(score), True, yellow)
+    window.blit(value, [0, 0])
+
+
+def draw_snake(snake_block, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(window, black, [x[0], x[1], snake_block, snake_block])
+
+
+def message(msg, color):
+    mesg = font_style.render(msg, True, color)
+    window.blit(mesg, [width / 6, height / 3])
+
+
+def gameLoop():
+    game_over = False
+    game_close = False
+
+    x = width / 2
+    y = height / 2
+
+    dx = 0
+    dy = 0
+
+    snake_list = []
+    length = 1
+
+    food_x = round(random.randrange(0, width - snake_block) / 10.0) * 10.0
+    food_y = round(random.randrange(0, height - snake_block) / 10.0) * 10.0
+
+    while not game_over:
+
+        while game_close:
+            window.fill(blue)
+            message("Game Over! Press C-Continue or Q-Quit", red)
+            score(length - 1)
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        game_over = True
+                        game_close = False
+                    if event.key == pygame.K_c:
+                        gameLoop()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    dx = -snake_block
+                    dy = 0
+                elif event.key == pygame.K_RIGHT:
+                    dx = snake_block
+                    dy = 0
+                elif event.key == pygame.K_UP:
+                    dy = -snake_block
+                    dx = 0
+                elif event.key == pygame.K_DOWN:
+                    dy = snake_block
+                    dx = 0
+
+        # Хана мөргөх
+        if x >= width or x < 0 or y >= height or y < 0:
+            game_close = True
+
+        x += dx
+        y += dy
+        window.fill(blue)
+        pygame.draw.rect(window, green, [food_x, food_y, snake_block, snake_block])
+
+        snake_head = []
+        snake_head.append(x)
+        snake_head.append(y)
+        snake_list.append(snake_head)
+
+        if len(snake_list) > length:
+            del snake_list[0]
+
+        # Өөрийгөө мөргөх
+        for block in snake_list[:-1]:
+            if block == snake_head:
+                game_close = True
+
+        draw_snake(snake_block, snake_list)
+        score(length - 1)
+
+        pygame.display.update()
+
+        # Хоол идэх
+        if x == food_x and y == food_y:
+            food_x = round(random.randrange(0, width - snake_block) / 10.0) * 10.0
+            food_y = round(random.randrange(0, height - snake_block) / 10.0) * 10.0
+            length += 1
+
+        clock.tick(snake_speed)
+
+    pygame.quit()
+    quit()
+
+
+gameLoop()
